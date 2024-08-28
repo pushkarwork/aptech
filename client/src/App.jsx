4ff675fce8e5edcfe75329d467aa2724cfc4b738
pushkar verma <pushkar753@gmail.com>
2024-08-29 01:01:43 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Layout/Header';
+import { BrowserRouter } from 'react-router-dom';
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Header />
+
+    </BrowserRouter>
+  )
+}
+
+export default App
